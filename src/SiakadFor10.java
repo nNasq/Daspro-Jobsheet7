70181ad63f8fb57adbbd4b1e98b4a817024d41a5
HafizhArrasyiidS <hafizharrasyiids@gmail.com>
2025-10-22 08:11:24 +0700

    Pertanyaan 1

--- src/SiakadFor10.java
+++ src/SiakadFor10.java
@@ -1,23 +1,34 @@
 import java.util.Scanner;
 
-public class  SiakadFor10 {
+public class SiakadFor10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
-        
+        int lulus = 0, tidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // Hitung lulus dan tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
-        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-    }   
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+    }
 }
